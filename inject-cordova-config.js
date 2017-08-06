1c24e8586b6560a6b834798d2fc791aa2a47cfdf
Ronny Roeller <ronny@collaborne.com>
2017-08-06 19:27:43 +0200

    Initial commit

---
+++ inject-cordova-config.js
@@ -0,0 +1,30 @@
+/**
+ * Injects values into the placeholders in Cordova config.xml file
+ */
+'use strict';
+
+const filter = require('gulp-filter');
+const lazypipe = require('lazypipe');
+const replace = require('gulp-replace');
+const size = require('gulp-size');
+
+const DEFAULT_SOURCE = 'index.html';
+
+module.exports = (appId, versionNumber, source = DEFAULT_SOURCE) => {
+	// Split version into major/minor/patch, and calculate a version code for android versioning
+	const versionTokens = versionNumber.split('.');
+	const androidVersionCode = versionTokens[0]*100000 + versionTokens[1]*1000 + versionTokens[2]*10
+
+	const configFilter = filter('config.xml', {restore: true});
+
+	return lazypipe()
+		.pipe(() => configFilter)
+		.pipe(() => replace('%%GULP.CORDOVA_ID%%', appId))
+		.pipe(() => replace('%%GULP.CORDOVA_VERSION%%', versionNumber))
+		.pipe(() => replace('%%GULP.CORDOVA_VERSION_ANDROID_VERSION_CODE%%', androidVersionCode))
+		.pipe(() => replace('%%GULP.CORDOVA_SOURCE%%', source))
+		.pipe(() => replace('%%GULP.CORDOVA_USER_AGENT%%', `${appId}/${versionNumber}`))
+		.pipe(() => replace('%%GULP.CORDOVA_ALLOWED_NAV_SOURCE%%', source !== DEFAULT_SOURCE ? source : 'file://'))
+		.pipe(() => size({title: 'inject-cordova-config'}))
+		.pipe(() => configFilter.restore);
+}
---
+++ test/inject-cordova-config/index.js
@@ -0,0 +1,60 @@
+/* jshint node: true */
+
+'use strict';
+
+const injectCordovaConfig = require('../../inject-cordova-config.js');
+const fs = require('fs');
+const gulp = require('gulp');
+const path = require('path');
+const should = require('should');
+const File = require('vinyl');
+
+describe('gulp-inject-cordova-config', function() {
+
+	let check;
+
+	beforeEach(function() {
+		const file = new File({
+			path: 'config.xml',
+			contents: fs.readFileSync(path.join(__dirname, 'fixtures/config.xml'))
+		});
+
+		// Function to collects all created files
+		check = function(stream, done, cb) {
+			const newFiles = {};
+			stream.on('data', function(newFile) {
+				newFiles[newFile.path] = newFile;
+			});
+			stream.on('end', function(newFile) {
+				cb(newFiles);
+				done();
+			});
+
+			stream.write(file);
+			stream.end();
+		};
+	});
+
+	it('injects data for placeholders (with default source)', function(done) {
+		const stream = injectCordovaConfig('com.company.app', '1.2.3')();
+
+		check(stream, done, function(files) {
+			const expected = fs.readFileSync(path.join(__dirname, 'expected/config-default-source.xml'), {encoding: 'utf8'});
+
+			const transformedFile = files['config.xml'];
+			String(transformedFile.contents).should.equal(expected);
+		});
+	});
+
+	it('injects data for placeholders (with custom source)', function(done) {
+		const stream = injectCordovaConfig('com.company.app', '1.2.3', 'http://localhost:41234')();
+
+		check(stream, done, function(files) {
+			const expected = fs.readFileSync(path.join(__dirname, 'expected/config-custom-source.xml'), {encoding: 'utf8'});
+
+			const transformedFile = files['config.xml'];
+			String(transformedFile.contents).should.equal(expected);
+		});
+	});
+
+});

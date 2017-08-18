850a42bbc31a312a9ec4695ea530b021eb1f2579
Ronny Roeller <ronny@collaborne.com>
2017-08-18 11:21:09 +0200

    Format code

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ exports.injectConfig = require('./inject-cordova-config.js');
 exports.injectIndex = require('./inject-cordova-index.js');
 exports.build = require('./build-cordova.js').build;
 exports.addYargs = require('./build-cordova.js').addYargs;
+

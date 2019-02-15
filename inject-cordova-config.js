e0feed6218e5ae80a30d55d74d51aefc3b7baf48
Andreas Kohn <andreas.kohn@gmail.com>
2019-02-15 14:01:03 +0100

    Fix type-related errors
    * Document options.versionNumber correctly
    * Add explicit Number/String conversions to help tsc work out what is happening here
    
    Neither of these changes should have any functional impact.

--- inject-cordova-config.js
+++ inject-cordova-config.js
@@ -15,13 +15,13 @@ const DEFAULT_SOURCE = 'index.html';
  * 
  * @param {Object} options Configuration for the replacement
  * @param {string} options.appId ID under which the app is registered in the app store, e.g. com.company.app
- * @param {string[]} options.versionNumber Version number of the release
+ * @param {string} options.versionNumber Version number of the release
  * @param {string} options.source Path of the application, e.g. index.html (or http://localhost:post for iOS)
  */
 module.exports = function injectConfig({ appId, versionNumber, source = DEFAULT_SOURCE} = options) {
 	// Split version into major/minor/patch, and calculate a version code for android versioning
 	const versionTokens = versionNumber.split('.');
-	const androidVersionCode = versionTokens[0]*100000 + versionTokens[1]*1000 + versionTokens[2]*10
+	const androidVersionCode = Number(versionTokens[0])*100000 + Number(versionTokens[1])*1000 + Number(versionTokens[2])*10;
 
 	const configFilter = filter('config.xml', {restore: true});
 
@@ -29,7 +29,7 @@ module.exports = function injectConfig({ appId, versionNumber, source = DEFAULT_
 		.pipe(() => configFilter)
 		.pipe(() => replace('%%GULP.CORDOVA_ID%%', appId))
 		.pipe(() => replace('%%GULP.CORDOVA_VERSION%%', versionNumber))
-		.pipe(() => replace('%%GULP.CORDOVA_VERSION_ANDROID_VERSION_CODE%%', androidVersionCode))
+		.pipe(() => replace('%%GULP.CORDOVA_VERSION_ANDROID_VERSION_CODE%%', String(androidVersionCode)))
 		.pipe(() => replace('%%GULP.CORDOVA_SOURCE%%', source))
 		.pipe(() => replace('%%GULP.CORDOVA_USER_AGENT%%', `${appId}/${versionNumber}`))
 		.pipe(() => replace('%%GULP.CORDOVA_ALLOWED_NAV_SOURCE%%', source !== DEFAULT_SOURCE ? source : 'file://'))
